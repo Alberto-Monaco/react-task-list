47df7f58ef02bae2a1b8a8ba925b5074d44a88ff
Alberto Monaco <alberto.monaco84@gmail.com>
2024-11-11 14:08:13 +0100

    import component generic

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
+import AppHeader from './components/AppHeader'
+import AppMain from './components/AppMain'
+import AppFooter from './components/AppFooter'
+
 function App() {
-	return <></>
+	return (
+		<>
+			<AppHeader />
+			<AppMain />
+			<AppFooter />
+		</>
+	)
 }
 
 export default App
